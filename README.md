2b676c86f0478d9c11816ad24014a7f8b26a0fdb
André Dalcin <andre@planosassessoria.com.br>
2019-09-14 00:38:19 -0300

    readme.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# VLBTC
+Fiz isso apenas para eu pegar o valor do BTC!
+
+### Libs
+[jq](https://stedolan.github.io/jq/)
--- btc.sh
+++ btc.sh
@@ -2,6 +2,6 @@
 #
 
 /usr/bin/curl -s -X GET https://api.bitvalor.com/v1/ticker.json | \
-    /usr/bin/jq .ticker_24h.total.last | \
+    ${HOME}/bin/jq .ticker_24h.total.last | \
     /usr/bin/xargs printf "%'.2f\n" | \
     /usr/bin/xargs /usr/bin/notify-send -u 'normal' -a 'Bitcoin' -i bitcoin128 'Valor do BTC Agora'
